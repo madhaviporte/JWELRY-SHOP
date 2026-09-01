14bc74bd3aa8164d43c71ba3edd9894d279cc514
madhaviporte <madhaviporte2004@gmail.com>
2026-09-01 15:29:00 +0530

    ready for deploy

--- TMS/Team-Management-System/Backend/config/db.js
+++ TMS/Team-Management-System/Backend/config/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
     process.exit(1);
